edaea02188eb07666198f6d9d1ca417e931c90d3
chenguanxi <chenguanxi@corp.netease.com>
2020-10-31 19:27:40 +0800

    feat: Element creation and JSX

---
+++ react-mini.js
@@ -0,0 +1,62 @@
+const domRoot = document.getElementById("root");
+
+const domInput = document.createElement("input");
+// Set properties
+domInput["type"] = "text";
+domInput["value"] = "Hi world";
+domInput["className"] = "my-class";
+// Listen to events
+domInput.addEventListener("change", (e) => alert(e.target.value));
+// Create a text node
+const domText = document.createTextNode("");
+// Set text node content
+domText["nodeValue"] = "Foo";
+// Append an element
+domRoot.appendChild(domInput);
+// Append a text node (same as previous)
+domRoot.appendChild(domText);
+
+function render(element, parentDom) {
+  const { type, props } = element;
+
+  // create DOM element
+  const isTextElement = type === 'TEXT ELEMENT';
+
+  const dom = isTextElement ? document.createTextNode("") : document.createElement(type);
+
+  const isListener = (name) => name.startWith("on");
+  Object.keys(props)
+    .filter(isListener)
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, props[name]);
+    });
+
+  const isAttribute = (name) => !isListener(name) && name !== "children";
+  Object.keys(props)
+    .filter(isAttribute)
+    .forEach((name) => {
+      dom[name] = props[name];
+    });
+
+  const childElements = props.chilren || [];
+  childElements.forEach((childElements) => render(childElements, dom));
+
+  parentDom.appendChild(dom);
+}
+
+const TEXT_ELEMENT = "TEXT ELEMENT";
+
+function createElement(type, config, ...args) {
+  const props = Object.assign({}, config);
+  const hasChildren = args.length > 0;
+  const rawChildren = hasChildren ? [].concat(...args) : [];
+  props.children = rawChildren
+    .filter(c => c !== null && c!== false)
+    .map(c => c instanceof Object ? c : createTextElement(c));
+  return { type, props };
+}
+
+function createTextElement(value) {
+  return createElement(TEXT_ELEMENT, { nodeValue: value });
+}
