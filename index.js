82e639f547fd7723373ffcd2ddab576734e9ac30
chenguanxi <chenguanxi@corp.netease.com>
2020-10-31 21:09:01 +0800

    feat: Instances, reconciliation and virtual DOM

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 /** @jsx ReactMini.createElement */
 import * as ReactMini from './react-mini';
-console.log(ReactMini)
+const randomLikes = () => Math.ceil(Math.random() * 100);
 
 const stories = [
-  { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
-  { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
-  { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
-  { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
-  { name: "Components and state", url: "http://bit.ly/2rE16nh" }
+  {
+    name: "Didact introduction",
+    url: "http://bit.ly/2pX7HNn",
+    likes: randomLikes()
+  },
+  {
+    name: "Rendering DOM elements ",
+    url: "http://bit.ly/2qCOejH",
+    likes: randomLikes()
+  },
+  {
+    name: "Element creation and JSX",
+    url: "http://bit.ly/2qGbw8S",
+    likes: randomLikes()
+  },
+  {
+    name: "Instances and reconciliation",
+    url: "http://bit.ly/2q4A746",
+    likes: randomLikes()
+  },
+  {
+    name: "Components and state",
+    url: "http://bit.ly/2rE16nh",
+    likes: randomLikes()
+  }
 ];
 
-const appElement = <div><ul>{ stories.map(story => storyElement(story))}</ul></div>;
+const appElement = () => <div><ul>{stories.map(storyElement)}</ul></div>;
 
-function storyElement({ name, url }) {
-  const likes = Math.ceil(Math.random() * 100);
+function storyElement(story) {
   return (
     <li>
-      <button>{likes}</button>
-      <a href={url} >{name}</a>
+      <button onClick={e => handleClick(story)}>{story.likes}<b>❤️</b></button>
+      <a href={story.url}>{story.name}</a>
     </li>
-  )
+  );
 }
 
-ReactMini.render(appElement, document.getElementById('root'))
+function handleClick(story) {
+  story.likes += 1;
+  ReactMini.render(appElement(), document.getElementById('root'))
+}
+
+ReactMini.render(appElement(), document.getElementById('root'))
--- react-mini.js
+++ react-mini.js
@@ -1,50 +1,3 @@
-// const domRoot = document.getElementById("root");
-
-// const domInput = document.createElement("input");
-// // Set properties
-// domInput["type"] = "text";
-// domInput["value"] = "Hi world";
-// domInput["className"] = "my-class";
-// // Listen to events
-// domInput.addEventListener("change", (e) => alert(e.target.value));
-// // Create a text node
-// const domText = document.createTextNode("");
-// // Set text node content
-// domText["nodeValue"] = "Foo";
-// // Append an element
-// domRoot.appendChild(domInput);
-// // Append a text node (same as previous)
-// domRoot.appendChild(domText);
-
-export function render(element, parentDom) {
-  const { type, props } = element;
-
-  // create DOM element
-  const isTextElement = type === 'TEXT ELEMENT';
-
-  const dom = isTextElement ? document.createTextNode("") : document.createElement(type);
-
-  const isListener = (name) => name.startsWith("on");
-  Object.keys(props)
-    .filter(isListener)
-    .forEach((name) => {
-      const eventType = name.toLowerCase().substring(2);
-      dom.addEventListener(eventType, props[name]);
-    });
-
-  const isAttribute = (name) => !isListener(name) && name !== "children";
-  Object.keys(props)
-    .filter(isAttribute)
-    .forEach((name) => {
-      dom[name] = props[name];
-    });
-
-  const childElements = props.children || [];
-  childElements.forEach((childElements) => render(childElements, dom));
-
-  parentDom.appendChild(dom);
-}
-
 const TEXT_ELEMENT = "TEXT ELEMENT";
 
 export function createElement(type, config, ...args) {
@@ -61,5 +14,99 @@ function createTextElement(value) {
   return createElement(TEXT_ELEMENT, { nodeValue: value });
 }
 
+
+let rootInstance = null;
+
+export function render(element, container) {
+  const prevInstance = rootInstance;
+  const nextInstance = reconcile(container, prevInstance, element);
+  rootInstance = nextInstance;
+}
+
+function reconcile(parentDom, instance, element) {
+  if (instance == null) {
+    // Create instance
+    const newInstance = instantiate(element);
+    parentDom.appendChild(newInstance.dom);
+    return newInstance;
+  } else if (element == null) {
+    // Remove instance
+    parentDom.removeChild(instance.dom);
+    return null;
+  } else if (instance.element.type === element.type) {
+    // Update instance
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.childInstances = reconcileChildren(instance, element);
+    instance.element = element;
+    return instance;
+  } else {
+    // Replace instance
+    const newInstance = instantiate(element);
+    parentDom.replaceChild(newInstance.dom, instance.dom);
+    return newInstance;
+  }
+}
+
+function reconcileChildren(instance, element) {
+  const dom = instance.dom;
+  const childInstances = instance.childInstances;
+  const nextChildElements = element.props.children || [];
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+    const newChildInstance = reconcile(dom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+  return newChildInstances.filter(instance => instance != null);
+}
+
+function instantiate(element) {
+  const { type, props } = element;
+
+  // create DOM element
+  const isTextElement = type === 'TEXT ELEMENT';
+
+  const dom = isTextElement ? document.createTextNode("") : document.createElement(type);
+
+  updateDomProperties(dom, [], props);
+
+    //Instantiate and append children
+    const childElement = props.children || [];
+    const childInstances = childElement.map(instantiate);
+    const childDoms = childInstances.map(childInstance => childInstance.dom);
+    childDoms.forEach(childDom => dom.appendChild(childDom));
+
+    const instance = { dom, element, childInstances };
+    return instance;
+}
+
+function updateDomProperties(dom, prevProps, nextProps) {
+  const isEvent = name => name.startsWith("on");
+  const isAttribute = name => !isEvent(name) && name != "children";
+
+  // Remove event listeners
+  Object.keys(prevProps).filter(isEvent).forEach(name => {
+    const eventType = name.toLowerCase().substring(2);
+    dom.removeEventListener(eventType, prevProps[name]);
+  });
+
+  // Remove attributes
+  Object.keys(prevProps).filter(isAttribute).forEach(name => {
+    dom[name] = null;
+  });
+
+  // Set attributes
+  Object.keys(nextProps).filter(isAttribute).forEach(name => {
+    dom[name] = nextProps[name];
+  });
+
+  // Add event listeners
+  Object.keys(nextProps).filter(isEvent).forEach(name => {
+    const eventType = name.toLowerCase().substring(2);
+    dom.addEventListener(eventType, nextProps[name]);
+  });
+}
 
 export default {}
